f5dd726ee1c05d415b01cfb251bc9c96225639e4
pramitasari <105182828+pramitasari@users.noreply.github.com>
2022-05-09 10:07:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>www.webreal.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to Hengker!" />
+        <h1>SELAMAT DATANG</h1>
         <p className="description">
-          Helo welcome to hangkerkawe, you are monitored!!
+          Helo welcome hangkerkawe, you are monitored!!
         </p>
       </main>
 
